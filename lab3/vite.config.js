c8d2db308b1499a49c1e117ed767a99507d9e94b
Wojciech Ptak <ptakwojt@gmail.com>
2025-05-05 03:59:22 +0200

    Hosting to fix

--- lab3/vite.config.js
+++ lab3/vite.config.js
@@ -1,7 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
+  resolve: {
+    alias: {
+      '/app': path.resolve(__dirname, 'app')
+    }
+  },
+  build: {
+    outDir: 'build/client'
+  }
 });
